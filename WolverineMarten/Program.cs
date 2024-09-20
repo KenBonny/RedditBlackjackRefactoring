edb7875f711fa7f39768229e7df79ab6f7bccb3e
Ken Bonny <bonny.ken+github@gmail.com>
2024-09-20 11:00:45 +0200

    Add WolverineFx packages and configure Wolverine in Program.cs
    Added WolverineFx and WolverineFx.Http package references to the project. Updated Program.cs to configure Wolverine with transactional policies and durable local queues, and integrated Oakton command handling.

--- WolverineMarten/Program.cs
+++ WolverineMarten/Program.cs
@@ -1,9 +1,23 @@
+using Oakton;
+using Wolverine;
+using Wolverine.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+builder.Host.UseWolverine(
+    opts =>
+    {
+        // This middleware will apply to the HTTP
+        // endpoints as well
+        opts.Policies.AutoApplyTransactions();
+
+        // Setting up the outbox on all locally handled
+        // background tasks
+        opts.Policies.UseDurableLocalQueues();
+    });
 
 var app = builder.Build();
 
@@ -15,4 +29,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.Run();
+app.MapWolverineEndpoints();
+
+await app.RunOaktonCommands(args);
