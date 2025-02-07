f767cabbf60c576575116fc32e3e70085676de01
Ken Bonny <bonny.ken+github@gmail.com>
2025-02-07 11:28:28 +0100

    Handle events in projection

--- WolverineMarten/Comments/Comment.cs
+++ WolverineMarten/Comments/Comment.cs
@@ -5,6 +5,48 @@ public record Thread
     public Guid Id { get; init; }
     public required string Title { get; init; }
     public List<Comment> Comments { get; init; } = [];
+
+    public static Thread Create(StartTopic start) =>
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Title = start.Title,
+            Comments =
+            [
+                new Comment
+                {
+                    Id = 0,
+                    Text = start.Text,
+                }
+            ]
+        };
+
+    public static Thread Apply(Reply reply, Thread thread)
+    {
+        thread.Comments.Add(
+            new()
+            {
+                Id = thread.Comments.Count,
+                Text = reply.Text,
+            });
+        return thread;
+    }
+
+    public static Thread Apply(Upvote upvote, Thread thread)
+    {
+        var comment = thread.Comments.SingleOrDefault(c => c.Id == upvote.Id);
+        if  (comment is not null)
+            comment.Upvotes++;
+        return thread;
+    }
+
+    public static Thread Apply(Downvote downvote, Thread thread)
+    {
+        var comment = thread.Comments.SingleOrDefault(c => c.Id == downvote.Id);
+        if (comment is not null)
+            comment.Downvotes++;
+        return thread;
+    }
 }
 
 public record Comment
@@ -13,8 +55,8 @@ public record Comment
     public required string Text { get; init; }
     public int? ParentId { get; init; }
     public Comment? Parent { get; init; }
-    public int Upvotes { get; init; }
-    public int Downvotes { get; init; }
+    public int Upvotes { get; set; } = 0;
+    public int Downvotes { get; set; } = 0;
 }
 
 public record StartTopic(string Title, string Text);
