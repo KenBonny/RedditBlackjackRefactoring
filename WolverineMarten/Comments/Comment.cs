ef96c1dde0564a7ea7d86f31025beed780d181b1
Ken Bonny <bonny.ken+github@gmail.com>
2025-02-07 10:50:47 +0100

    Add events and projections classes

---
+++ WolverineMarten/Comments/Comment.cs
@@ -0,0 +1,28 @@
+﻿namespace WolverineMarten.Comments;
+
+public record Thread
+{
+    public Guid Id { get; init; }
+    public required string Title { get; init; }
+    public List<Comment> Comments { get; init; } = [];
+}
+
+public record Comment
+{
+    public int Id { get; init; }
+    public required string Text { get; init; }
+    public int? ParentId { get; init; }
+    public Comment? Parent { get; init; }
+    public int Upvotes { get; init; }
+    public int Downvotes { get; init; }
+}
+
+public record StartTopic(string Title, string Text);
+
+public record CloseTopic(int Id);
+
+public record Reply(int ParentId, string Text);
+
+public record Upvote(int Id);
+
+public record Downvote(int Id);
