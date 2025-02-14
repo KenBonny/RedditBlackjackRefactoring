3905bc6d1353768c97e2d5312fbdea499c828525
Ken Bonny <bonny.ken+github@gmail.com>
2025-02-14 11:29:21 +0100

    Add endpoint to fetch all topics in GetThreadHandler
    Introduced a new WolverineGet endpoint at "/topic" to retrieve all topics. This utilizes Marten's IQuerySession to fetch and return the list of threads asynchronously.

--- WolverineMarten/Comments/GetThreadHandler.cs
+++ WolverineMarten/Comments/GetThreadHandler.cs
@@ -1,10 +1,14 @@
-﻿using Wolverine.Http;
+﻿using Marten;
+using Wolverine.Http;
 using Wolverine.Http.Marten;
 
 namespace WolverineMarten.Comments;
 
 public static class GetThreadHandler
 {
+    [WolverineGet("/topic")]
+    public static Task<IReadOnlyList<Thread>> GetTopics(IQuerySession session) => session.Query<Thread>().ToListAsync();
+
     [WolverineGet("/topic/{topicId}")]
     public static IResult GetTopic([Aggregate("topicId")] Thread thread) => Results.Ok(thread);
 }
