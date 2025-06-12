428eef68a691db0f719fd33a9e0e15efdadbe614
Ken Bonny <bonny.ken+github@gmail.com>
2025-06-12 12:17:47 +0200

    Add benchmark project for DoubleQuote vs string.Empty analysis
    Introduced a new project, `BenchmarkExperiments`, to analyze performance differences between using double quotes (`""`) and `string.Empty`. Configured BenchmarkDotNet for running benchmarks on .NET 4.6.2 and .NET 9.0, with results exported in RPlot format. This provides insights into string allocation performance.

---
+++ BenchmarkExperiments/DoubleQuoteVsEmpty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace BenchmarkExperiments
+{
+    [SimpleJob(RuntimeMoniker.Net462)]
+    [SimpleJob(RuntimeMoniker.Net90)]
+    [RPlotExporter]
+    public class DoubleQuoteVsEmpty
+    {
+        private List<string> strings;
+
+        [Params(10_000_000, 1_000_000, 10_000)]
+        public int Iterations;
+
+        [GlobalSetup]
+        public void Setup() => strings = new List<string>(Iterations);
+
+        [IterationCleanup]
+        public void Cleanup() => strings.Clear();
+
+        [Benchmark]
+        public List<string> DoubleQuoteStrings()
+        {
+            for (int index = 0; index < Iterations; index++)
+            {
+                strings.Add("");
+            }
+            return strings;
+        }
+
+        [Benchmark]
+        public List<string> EmptyStrings()
+        {
+            for (int index = 0; index < Iterations; index++)
+            {
+                strings.Add(string.Empty);
+            }
+
+            return strings;
+        }
+    }
+}
---
+++ BenchmarkExperiments/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using BenchmarkExperiments;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var summary = BenchmarkRunner.Run<DoubleQuoteVsEmpty>();
+    }
+}
