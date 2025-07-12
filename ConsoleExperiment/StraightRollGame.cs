52d7e1ac592a0cc2d3ba0c64ba719a5dc44164b4
Ken Bonny <bonny.ken+github@gmail.com>
2025-07-12 11:44:53 +0200

    Add base game loop

---
+++ ConsoleExperiment/StraightRollGame.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleExperiment;
+
+public static class StraightRollGame
+{
+    public static void Play()
+    {
+        do
+        {
+            // While R key is true, loop through 6 iterations of a random number gen. Each RNG
+            // loop, another 6 loop runs (36 potential loops total) to check if each value added to
+            // our list "diceRoll" == the predefined values of our list "diceStraight". If so,
+            // boolean "rolledStraight" is true, and our integer tally "straightCheck" gets added to,
+            // else straightCheck is nullified for that loop. If the tally reaches or exceeds 6,
+            // the user has a straight.
+            Console.WriteLine("Press R to roll...\n");
+            ConsoleKeyInfo rollKey = Console.ReadKey();
+
+            Random RNG = new Random();
+            int straightCheck = 0;
+            List<int> diceStraight = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6
+            };
+            List<int> diceRoll = new List<int>(6);
+            switch (rollKey.Key)
+            {
+                case ConsoleKey.R:
+                    Console.WriteLine("\n");
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int rand = RNG.Next(1, 7);
+                        diceRoll.Add(rand);
+
+                        for (int n = 0; n < 6; n++)
+                        {
+                            bool rolledStraight = diceRoll.Contains(diceStraight[n]);
+                            if (rolledStraight == true)
+                            {
+                                straightCheck++;
+                                //Console.WriteLine($"i={i} n={n} Straight Check={straightCheck}");
+                            }
+                            else if (rolledStraight == false)
+                            {
+                                straightCheck = 0;
+                                Console.WriteLine("________RE-SET________");
+                            }
+                        }
+                    }
+
+                    if (straightCheck >= 6)
+                    {
+                        Console.WriteLine(">_>_>_STRAIGHT_<_<_<");
+                    }
+
+                    foreach (int number in diceRoll)
+                    {
+                        Console.WriteLine($" dice: {number}");
+                    }
+
+                    break;
+            }
+        } while (true);
+    }
+}
